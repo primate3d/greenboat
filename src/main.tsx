1afd6b67e3e6ca867870ce7da299be8afcd46c2a
primate3d <yanntejou@ik.me>
2026-03-06 10:22:08 +0000

    add vite index entry

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <h1>Bateau Vert - RDV Brevo prêt !</h1>
+  </React.StrictMode>,
+)
